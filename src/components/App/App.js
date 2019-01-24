10e5a7d8c7dddbe36ec65c878a75bb46b5a0e903
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-01-24 00:16:07 -0700

    Add fetch for nested residents array in planets

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,14 +9,17 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      people: []
+      people: [],
+      planets: []
     }
   }
 
   componentDidMount = () => {
     this.fetchPeople();
+    this.fetchPlanets();
   }
 
+  // peoples fetch
   fetchPeople = async () => {
     let fetchedData = [];
     for (let i = 1; i < 10; i++) {
@@ -34,8 +37,7 @@ export default class App extends Component {
     const unresolvedPromises = fetchedPeople.map(async person => {
       const response = await fetch(person.homeworld);
       const result = await response.json();
-      return ({ ...person, homeworld: result.name, population: result.population 
-      })
+      return ({ ...person, homeworld: result.name, population: result.population })
     })
     return Promise.all(unresolvedPromises);
 }
@@ -45,13 +47,69 @@ export default class App extends Component {
       if (person.species.length > 0) {
         const response = await fetch(person.species[0])
         const result = await response.json();
-        return ({ person: person.name, homeworld: person.homeworld, population: person.population, species: result.name});
+        return ({ person: person.name, homeworld: person.homeworld, population: person.population, species: result.name, language: result.language});
       } else {
-        return ({ person: person.name, homeworld: person.homeworld, population: person.population, species: 'Unknown'});
+        return ({ person: person.name, homeworld: person.homeworld, population: person.population, species: 'Unknown', language: 'Unknown'});
       }
     })
     return Promise.all(unresolvedPromises);
   }
+
+
+
+
+
+
+
+
+  // planets fetch
+  
+  fetchPlanets = async () => {
+    let fetchedPlanetData = [];
+    for (let i = 1; i < 8; i++) {
+      let url = `https://swapi.co/api/planets/?page=${i}`
+      const response = await fetch(url);
+      const result = await response.json();
+      fetchedPlanetData.push(...result.results);
+    }
+    const checkedPlanets = await this.fetchResidents(fetchedPlanetData);
+    this.setState({ planets: checkedPlanets });
+  }
+
+
+
+
+  fetchResidents = (planets) => {
+    const unresolvedPromises = planets.map(async planet => {
+      if (planet.residents.length > 0) {
+        let newArray = [];
+        for (let i = 0; i < planet.residents.length; i++) {
+          const response = await fetch(planet.residents[i])
+          const result = await response.json()
+          newArray.push(result.name);
+        }
+        return ({ planet: planet.name, terrain: planet.terrain, population: planet.population, climate: planet.climate, residents: newArray})
+        } else {
+        return ({ planet: planet.name, terrain: planet.terrain, population: planet.population, climate: planet.climate, residents: 'None.'})
+        }
+
+      })
+    return Promise.all(unresolvedPromises)
+  }
+
+  pushMultipleResidents = (residents) => {
+    console.log(residents)
+    return residents;
+    // this.setState({ planets: checkedPlanets });
+
+  }
+
+
+
+
+
+
+  // vehicles fetch
 
   render() {
     return (

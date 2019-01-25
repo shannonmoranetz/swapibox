2e67b51ce541a3c6db5d40cad401e0b6357196e7
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-01-25 12:41:19 -0700

    Add active class to button state

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -4,51 +4,24 @@ export class Controls extends Component {
   constructor() {
     super();
     this.state = {
-      buttonIsClicked: false
+      active: null
     }
   }
 
   passCategory = (e) => {
     let category = e.target.name;
-    if (this.state.buttonIsClicked === false ) {
-      this.props.retrieveCategory(category)
-      this.setState({ buttonIsClicked: true })
-    } else {
-      this.displayClassButtons(category)
-    }
+    this.props.retrieveCategory(category)
+    this.setState({ active: e.target.name })
   }
-
-  displayAllButtons = () => {
-    let categories = ['people', 'planets', 'vehicles']
-    let allButtons = categories.map((category) => {
-        return <button className={`${this.state.buttonClicked}`} name={`${category}`} onClick={this.passCategory}>{`${category}`}</button>
-      })
-    return allButtons;
-  }
-
-
-
-  
-  displayClassButtons = (clickedValue) => {
-    let categories = ['people', 'planets', 'vehicles']
-
-    let Clicked = categories.filter((category) => {
-      return category === clickedValue
-    })
-
-    let unClicked = categories.filter((category) => {
-      return category !== clickedValue
-    })
-
-    return (Clicked, unClicked);
-  }
-
 
   render() {
-    if (this.state.buttonIsClicked === false) {
-      return this.displayAllButtons();
-    } else {
-      return this.displayClassButtons();
-    }
+    let categories = ['people', 'planets', 'vehicles']
+    return categories.map((category) => {
+      return <button active={this.state.isActive} 
+      className={
+        this.state.active === category ? 'active' : 'inactive'
+      }
+      name={`${category}`} onClick={this.passCategory}>{`${category}`}</button>
+    })
   }
 }

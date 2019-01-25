c9c5ab11b7b0128530481a58a013d474728ba661
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-01-25 01:08:59 -0700

    Map buttons in controls

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -4,7 +4,7 @@ export class Controls extends Component {
   constructor() {
     super();
     this.state = {
-
+      isClicked: false
     }
   }
 
@@ -13,12 +13,24 @@ export class Controls extends Component {
     this.props.retrieveCategory(category)
   }
 
+  changeButtonColor = () => {
+    this.setState({isClicked: !this.state.isClicked})
+ }
+
   render() {
+    let buttons;
+    let categories = ['people', 'planets', 'vehicles']
+    // if (this.state.isClicked) {
+      buttons = categories.map((category) => {
+      return <button className={`${category}`} name={`${category}`} onClick={this.passCategory}>{`${category}`}</button>
+      })
+    // }
     return(
       <div className="Controls">
-        <button className="people-button" name="people" onClick={this.passCategory}>People</button>
+        {/* <button className="people-button" name="people" onClick={this.passCategory}>People</button>
         <button className="planets-button" name="planets" onClick={this.passCategory}>Planets</button>
-        <button className="vehicles-button" name="vehicles" onClick={this.passCategory}>Vehicles</button>
+        <button className="vehicles-button" name="vehicles" onClick={this.passCategory}>Vehicles</button> */}
+        { buttons }
       </div>
     )
   }

8fe62924389324661fdd721e6632ad8ec2916aef
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-01-25 02:34:19 -0700

    Add further changes to controls methods

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -4,45 +4,51 @@ export class Controls extends Component {
   constructor() {
     super();
     this.state = {
-
+      buttonIsClicked: false
     }
   }
 
   passCategory = (e) => {
     let category = e.target.name;
-    this.checkClicked(e)
-    this.checkUnclicked(e)
-    this.props.retrieveCategory(category)
+    if (this.state.buttonIsClicked === false ) {
+      this.props.retrieveCategory(category)
+      this.setState({ buttonIsClicked: true })
+    } else {
+      this.displayClassButtons(category)
+    }
   }
 
-  checkClicked = (e) => {
+  displayAllButtons = () => {
     let categories = ['people', 'planets', 'vehicles']
+    let allButtons = categories.map((category) => {
+        return <button className={`${this.state.buttonClicked}`} name={`${category}`} onClick={this.passCategory}>{`${category}`}</button>
+      })
+    return allButtons;
+  }
+
+
+
+  
+  displayClassButtons = (clickedValue) => {
+    let categories = ['people', 'planets', 'vehicles']
+
     let Clicked = categories.filter((category) => {
-      return category === e.target.name
+      return category === clickedValue
     })
-    console.log('clicked: ', Clicked)
-    return Clicked;
+
+    let unClicked = categories.filter((category) => {
+      return category !== clickedValue
+    })
+
+    return (Clicked, unClicked);
   }
 
-  checkUnclicked = (e) => {
-    let categories = ['people', 'planets', 'vehicles']
-    let unClicked = categories.filter((category) => {
-      return category !== e.target.name
-    })
-    console.log('UNclicked: ', unClicked)
-    return unClicked;
-  }
 
   render() {
-    let categories = ['people', 'planets', 'vehicles']
-
-    let buttons = categories.map((category) => {
-        return <button className={`${this.state.buttonClicked}-active`} name={`${category}`} onClick={this.passCategory}>{`${category}`}</button>
-      })
-
-    return (
-      buttons
-    )
-
+    if (this.state.buttonIsClicked === false) {
+      return this.displayAllButtons();
+    } else {
+      return this.displayClassButtons();
+    }
   }
 }

670dfe38accdf492f33b9d67155108787621ba7c
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-01-24 18:39:16 -0700

    Add helper js and move api helper functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
-// SCROLLTEXT
+import { fetchScrollTextData, fetchHomeWorld, fetchSpecies, fetchResidents, fetchVehicleData } from './helpers.js';
 
 export const fetchScrollText = async () => {
   let fetchedScrollTextData = [];
@@ -9,15 +9,6 @@ export const fetchScrollText = async () => {
   const crawlData = await fetchScrollTextData(fetchedScrollTextData);
   return crawlData;
 }
-
-const fetchScrollTextData = async (films) => {
-  const unresolvedPromises = films.map(async film => {
-  return ({ title: film.title, date: film.release_date, crawl: film.opening_crawl })
-  })
-  return Promise.all(unresolvedPromises);
-}
-
-// PEOPLE
 
 export const fetchPeople = async () => {
   let fetchedPeopleData = [];
@@ -30,30 +21,6 @@ export const fetchPeople = async () => {
   return completePeopleData;
 }
 
-const fetchHomeWorld = (fetchedPeople) => {
-  const unresolvedPromises = fetchedPeople.map(async person => {
-    const response = await fetch(person.homeworld);
-    const result = await response.json();
-    return ({ ...person, homeworld: result.name, population: result.population })
-  })
-  return Promise.all(unresolvedPromises);
-}
-
-const fetchSpecies = (fetchedPeople) => {
-  const unresolvedPromises = fetchedPeople.map(async person => {
-    if (person.species.length > 0) {
-      const response = await fetch(person.species[0]);
-      const result = await response.json();
-      return ({ person: person.name, homeworld: person.homeworld, population: person.population, species: result.name, language: result.language});
-    } else {
-      return ({ person: person.name, homeworld: person.homeworld, population: person.population, species: 'Unknown', language: 'Unknown'});
-    }
-  })
-return Promise.all(unresolvedPromises);
-}
-
-// PLANETS
-
 export const fetchPlanets = async () => {
   let fetchedPlanetData = [];
   let url = `https://swapi.co/api/planets/?page=1`
@@ -64,29 +31,6 @@ export const fetchPlanets = async () => {
   return planetsData;
 }
 
-const fetchResidents = (planets) => {
-  const unresolvedPromises = planets.map(async planet => {
-    if (planet.residents.length > 0) {
-      const addedResidents = await addResidents(planet.residents);
-      return ({ planet: planet.name, terrain: planet.terrain, population: planet.population, climate: planet.climate, residents: addedResidents})
-      } else {
-      return ({ planet: planet.name, terrain: planet.terrain, population: planet.population, climate: planet.climate, residents: 'None.'})
-      }
-    })
-  return Promise.all(unresolvedPromises)
-}
-
-const addResidents = (links) => {
-  const unresolvedPromises = links.map(async link => {
-    const response = await fetch(link);
-    const result = await response.json();
-    return result.name;
-  })
-  return Promise.all(unresolvedPromises)
-}
-
-// VEHICLES
-
 export const fetchVehicles = async () => {
   let fetchedVehicleData = [];
   let url = `https://swapi.co/api/vehicles/?page=1`
@@ -95,11 +39,4 @@ export const fetchVehicles = async () => {
   fetchedVehicleData.push(...result.results);
   const vehicleData = await fetchVehicleData(fetchedVehicleData);
   return vehicleData;
-}
-
-const fetchVehicleData = async (vehicles) => {
-  const unresolvedPromises = vehicles.map(async vehicle => {
-  return ({ name: vehicle.name, model: vehicle.model, class: vehicle.vehicle_class, passengers: vehicle.passengers})
-  })
-  return Promise.all(unresolvedPromises);
 }
---
+++ src/helpers.js
@@ -0,0 +1,56 @@
+export const fetchScrollTextData = async (films) => {
+  const unresolvedPromises = films.map(async film => {
+  return ({ title: film.title, date: film.release_date, crawl: film.opening_crawl })
+  })
+  return Promise.all(unresolvedPromises);
+}
+
+export const fetchHomeWorld = (fetchedPeople) => {
+  const unresolvedPromises = fetchedPeople.map(async person => {
+    const response = await fetch(person.homeworld);
+    const result = await response.json();
+    return ({ ...person, homeworld: result.name, population: result.population })
+  })
+  return Promise.all(unresolvedPromises);
+}
+
+export const fetchSpecies = (fetchedPeople) => {
+  const unresolvedPromises = fetchedPeople.map(async person => {
+    if (person.species.length > 0) {
+      const response = await fetch(person.species[0]);
+      const result = await response.json();
+      return ({ person: person.name, homeworld: person.homeworld, population: person.population, species: result.name, language: result.language});
+    } else {
+      return ({ person: person.name, homeworld: person.homeworld, population: person.population, species: 'Unknown', language: 'Unknown'});
+    }
+  })
+return Promise.all(unresolvedPromises);
+}
+
+export const fetchResidents = (planets) => {
+  const unresolvedPromises = planets.map(async planet => {
+    if (planet.residents.length > 0) {
+      const addedResidents = await addResidents(planet.residents);
+      return ({ planet: planet.name, terrain: planet.terrain, population: planet.population, climate: planet.climate, residents: addedResidents})
+      } else {
+      return ({ planet: planet.name, terrain: planet.terrain, population: planet.population, climate: planet.climate, residents: 'None.'})
+      }
+    })
+  return Promise.all(unresolvedPromises)
+}
+
+const addResidents = (links) => {
+  const unresolvedPromises = links.map(async link => {
+    const response = await fetch(link);
+    const result = await response.json();
+    return result.name;
+  })
+  return Promise.all(unresolvedPromises)
+}
+
+export const fetchVehicleData = async (vehicles) => {
+  const unresolvedPromises = vehicles.map(async vehicle => {
+  return ({ name: vehicle.name, model: vehicle.model, class: vehicle.vehicle_class, passengers: vehicle.passengers})
+  })
+  return Promise.all(unresolvedPromises);
+}

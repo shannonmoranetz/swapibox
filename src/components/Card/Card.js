5f2256ab83bcd9fe0e8674b05a08510ade6e548f
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-01-25 22:02:47 -0700

    Add refactored basic functionality minus favorites

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,35 +9,78 @@ export class Card extends Component {
   }
 
   checkFavoriteStatus = () => {
+    console.log(this.props.category)
     this.setState({ isFavorited: !this.state.isFavorited })
     if (this.state.isFavorited === false) {
-      this.passFavorited(this.props.person)
+      this.passFavorited(this.props.item)
     } else {
-      this.passUnfavorited(this.props.person)
+      this.passUnfavorited(this.props.item)
     }
   }
 
-  passFavorited = (personData) => {
-    this.props.retrieveFavorited(personData)
+  passFavorited = (itemData) => {
+    this.props.retrieveFavorited(itemData)
   }
 
-  passUnfavorited = (personData) => {
-    this.props.removeFavorited(personData)
+  passUnfavorited = (itemData) => {
+    this.props.removeFavorited(itemData)
   }
 
   render() {
-    let { person } = this.props;
-    return(
-      <div className="Card">
-        <button className={
-                this.state.isFavorited === true ? 'active' : 'inactive'
-              }  onClick={this.checkFavoriteStatus}>favorite</button>
-        <p className="name">{person.person}</p>
-        <p className="homeworld">{person.homeworld}</p>
-        <p className="population">{person.population}</p>
-        <p className="species">{person.species}</p>
-        <p className="language">{person.language}</p>
-      </div>
-    )
+    let { item } = this.props;
+    if (this.props.category === 'people') {
+      return (
+        <div className="Card">
+          <button className={
+                  this.state.isFavorited === true ? 'active' : 'inactive'
+                }  onClick={this.checkFavoriteStatus}>favorite</button>
+          <p className="name">{item.person}</p>
+          <p className="homeworld">{item.homeworld}</p>
+          <p className="population">{item.population}</p>
+          <p className="species">{item.species}</p>
+          <p className="language">{item.language}</p>
+        </div>
+      )
+    } else if (this.props.category === 'planets') {
+      return (
+        <div className="Card">
+          <button className={
+                  this.state.isFavorited === true ? 'active' : 'inactive'
+                }  onClick={this.checkFavoriteStatus}>favorite</button>
+          <p className="name">{item.planet}</p>
+          <p className="homeworld">{item.terrain}</p>
+          <p className="population">{item.population}</p>
+          <p className="species">{item.climate}</p>
+          <p className="language">{item.addedResidents}</p>
+        </div>
+      )
+    } else if (this.props.category === 'vehicles') {
+      return (
+        <div className="Card">
+          <button className={
+                  this.state.isFavorited === true ? 'active' : 'inactive'
+                } 
+                onClick={this.checkFavoriteStatus}>favorite</button>
+          <p className="name">{item.name}</p>
+          <p className="homeworld">{item.model}</p>
+          <p className="population">{item.class}</p>
+          <p className="species">{item.passengers}</p>
+        </div>
+      )
+    } 
+    else if (this.props.category === 'favorites') {
+      return (
+        <div className="Card">
+          <button className={
+                  this.state.isFavorited === true ? 'active' : 'inactive'
+                }  onClick={this.checkFavoriteStatus}>favorite</button>
+          <p className="name">{item.person}</p>
+          <p className="homeworld">{item.homeworld}</p>
+          <p className="population">{item.population}</p>
+          <p className="species">{item.species}</p>
+          <p className="language">{item.language}</p>
+        </div>
+      )
+    }
   }
 }

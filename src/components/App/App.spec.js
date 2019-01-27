a9679a30b3dd369d4f36279144a49e8764a4bc1e
shannonmoranetz <shannonrmoranetz@gmail.com>
2019-01-27 14:02:02 -0700

    Add app test setup

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <App />
+    )
+  })
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+
+  it('should have the proper default state', () => {
+    expect(wrapper.state()).toEqual({
+      category: '',
+      crawl: [],
+      dataIsLoaded: null,
+      favorites: [],
+      people: [],
+      planets: [],
+      vehicles: []
+    })
+  })
+  
+})
